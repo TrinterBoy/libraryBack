c910e0003d1460af0447f89825e8f5005906061f
DudnykAlex <Oleksandr.Dudnyk@fivesysdev.com>
2024-06-12 19:42:33 +0300

    init

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,19 +1,27 @@
 const uuid = require('uuid')
 const path = require('path')
-const {Book, Basket} = require('../models/models')
+const { Book, Basket } = require('../models/models')
 const ApiError = require('../Errors/ApiError')
-const {Op} = require("sequelize");
+const { Op } = require("sequelize");
+const fs = require('fs/promises');
 
 
 class BookController {
     async create(req, res, next) {
         try {
-            const {name, author, year, genreId, desc} = req.body
-            const {img} = req.files
+            const { name, author, year, genreId, desc } = req.body
+            const { img, pdf } = req.files;
             let fileName = uuid.v4() + ".jpg"
-            img.mv(path.resolve(__dirname, '..', 'static', fileName))
 
-            const book = await Book.create({name, author, year, genreId, desc, img: fileName})
+            const book = await Book.create({ name, author, year, genreId, desc, img: fileName })
+
+            try {
+                const newPath = path.resolve(__dirname, '..', './static', `./${ book.id }`);
+                await fs.mkdir(newPath);
+                await pdf.mv(path.resolve(__dirname, '..', './static', `./${ book.id }`, `${ name }.pdf`))
+                await img.mv(path.resolve(__dirname, '..', 'static', fileName))
+            } catch (e) {
+            }
 
             return res.json(book)
 
@@ -24,36 +32,34 @@ class BookController {
     }
 
     async getAll(req, res) {
-        let {searchString, genreId, limit, page} = req.query
+        let { searchString, genreId, limit, page } = req.query
         page = page || 1
         limit = limit || 6
         let offset = page * limit - limit
         let book;
-        console.log(limit)
 
-        const where = {
-        }
+        const where = {}
         if (genreId) {
             where.genreId = genreId
         }
         if (searchString) {
             where.author = {
-                [Op.iLike]: `%${searchString}%`
+                [Op.iLike]: `%${ searchString }%`
             };
             where.name = {
-                [Op.iLike]: `%${searchString}%`
+                [Op.iLike]: `%${ searchString }%`
             };
         }
 
-        book = await Book.findAndCountAll({where, limit, offset})
+        book = await Book.findAndCountAll({ where, limit, offset })
         return res.json(book)
 
     }
 
     async getOne(req, res, next) {
         try {
-            const {id} = req.params
-            const book = await Book.findOne({where: {id}})
+            const { id } = req.params
+            const book = await Book.findOne({ where: { id } })
             return res.json(book)
         } catch (e) {
             next(ApiError.badRequest(e.message))
@@ -61,33 +67,33 @@ class BookController {
     }
 
     async getAllById(req, res) {
-        const {id} = req.query
-        const book = await Book.findAll({where: {id}})
+        const { id } = req.query
+        const book = await Book.findAll({ where: { id } })
         return res.json(book)
     }
 
     async postOneFalse(req, res) {
-        const {id} = req.body
+        const { id } = req.body
         const book = await Book.update({
             isA: false
         }, {
-            where: {id}
+            where: { id }
         })
         return res.json(book)
     }
 
     async postOneTrue(req, res) {
-        const {id} = req.body
+        const { id } = req.body
         const book = await Book.update({
             isA: true
         }, {
-            where: {id}
+            where: { id }
         })
         return res.json(book)
     }
 
     async getAllUnavailable(req, res) {
-        const book = await Book.findAll({where: {isA: false}})
+        const book = await Book.findAll({ where: { isA: false } })
         return res.json(book)
     }
 
